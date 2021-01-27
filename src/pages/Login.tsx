aa2c604a7887b581ab1768601375900ffe50e784
EduardoAraD <araujocarlos893@gmail.com>
2021-01-27 16:19:49 -0300

    Create page Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuth } from '../contexts/auth'
+
 import '../styles/login.css'
 
 export default function App () {
+  const { login } = useAuth()
+  const [name, setName] = useState('')
+
+  function handleSubmitLogin () {
+    console.log(name)
+    login(name)
+  }
+
   return (
-        <div id="page-login">
-            <h1>Login</h1>
-        </div>
+    <div id="page-login">
+      <div className='content'>
+        <h2>Seja bem vindo</h2>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button onClick={handleSubmitLogin}>Entrar</button>
+      </div>
+    </div>
   )
 }
