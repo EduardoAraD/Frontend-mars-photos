5926a3bb90aa4e9e9c3b15846792be8aa7ad2243
EduardoAraD <araujocarlos893@gmail.com>
2021-01-29 17:00:25 -0300

    Update Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { FiPower } from 'react-icons/fi'
 import { useAuth } from '../contexts/auth'
 
 import '../styles/components/header.css'
 
 export default function Header () {
-  const { user } = useAuth()
+  const { user, signOut } = useAuth()
   return (
         <header className="header">
             <h1>Olá {user.name}</h1>
+            <div className="signOut">
+              <button onClick={signOut}>
+                <FiPower color="#232323" size={20} />
+              </button>
+              <h3>Sair</h3>
+            </div>
         </header>
   )
 }
